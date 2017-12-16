8fcc0e3341bd73a2a0ace535479d1e83ce3fb830
vladborisov182 <vladborisov182@gmail.com>
2017-12-16 19:54:20 +0200

    navbar

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 class Configuration(object):
-    DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://esydispabrrdtx:74fe323264f1b9d38bdb43ce157dfba764ed23886a1abb6193927fe9168c87a3@ec2-184-72-228-128.compute-1.amazonaws.com:5432/de553hborc4cpf'
     CSRF_ENABLED = True
